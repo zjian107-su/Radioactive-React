1eff3f86fdff5c8d07722816f4d31bc79c677b3e
Zezheng Jiang <35544956+zjian107-su@users.noreply.github.com>
2023-07-11 17:54:16 -0400

    ✨update: function and class components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
 
-function App() {
+// arrow function
+const Body01 = () => (
+  <div className="App">
+    <h1>You are awesome</h1>
+  </div>
+);
+
+const Body02 = () => (
+  <div className="App">
+    <h1>You are best</h1>
+  </div>
+);
+
+// class declaration
+class ThirdBody extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.props.context}</h1>
+      </div>
+    );
+  }
+}
+
+const FourthBody = (props) => (
+  <div className="App">
+    <h1>{props.content}</h1>
+  </div>
+);
+
+// function declaration
+function Header() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Bombastic Bro</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
+    </div>
+  );
+}
+
+const Todos = () => (
+  <ul>
+    <li>Learn React</li>
+    <li>Learn Redux</li>
+    <li>Learn React Router</li>
+    <li>Learn React Native</li>
+  </ul>
+);
+
+function App() {
+  return (
+    <div>
+      {/* <Header /> */}
+      <Body01 content="content01" />
+      <Body02 value={5} />
+      <ThirdBody context="I am cool" />
+      <FourthBody content="I am cool too!" />
+      <Todos />
     </div>
   );
 }
